abbe7cbddf9cb4c19d31c7cb827fa6b655794370
thejp <jp_55555@hotmail.com>
2015-03-23 21:18:53 +0100

    Worked on lab02

---
+++ source/DotNetBay.WPF/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DotNetBay.Core;
+using DotNetBay.Data.FileStorage;
+using DotNetBay.Interfaces;
+using DotNetBay.Core.Execution;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IMainRepository MainRepository { private set; get; }
+        public IAuctionRunner AuctionRunner { private set; get; }
+
+        public App()
+        {
+            this.MainRepository = new FileSystemMainRepository("store.json");
+            this.AuctionRunner = new AuctionRunner(this.MainRepository);
+            this.AuctionRunner.Start();
+            //Test data
+            var memberService = new MemberService(this.MainRepository);
+            var service = new AuctionService(this.MainRepository);
+            if (!service.GetAll().Any())
+            {
+                var me = memberService.GetCurrentMember();
+                service.Save(new Auction
+                {
+                    Title = "My First Auction",
+                    StartDateTimeUtc = DateTime.UtcNow.AddSeconds(10),
+                    EndDateTimeUtc = DateTime.UtcNow.AddDays(14),
+                    StartPrice = 72,
+                    Seller = me
+                });
+            }
+        }
+    }
+}
---
+++ source/DotNetBay.WPF/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DotNetBay.Core;
+using DotNetBay.Model;
+
+namespace DotNetBay.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private readonly ObservableCollection<Auction> auctions = new ObservableCollection<Auction>();
+        public ObservableCollection<Auction> Auctions { get { return auctions; } } 
+
+        public MainWindow()
+        {
+            if (Application.Current is App)
+            {
+                AuctionService service = new AuctionService((Application.Current as App).MainRepository);
+                foreach (var auction in service.GetAll())
+                {
+                    Auctions.Add(auction);
+                }
+            }
+            DataContext = this;
+            this.InitializeComponent();
+        }
+    }
+}
